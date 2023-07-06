1ef1b905f846a1fd65394bf179e56958885cf9de
Alizeh Khan <alizehedinburgh@gmail.com>
2023-07-06 09:04:07 +0200

    feat: testimonial name

--- src/sections/Testimonial.tsx
+++ src/sections/Testimonial.tsx
@@ -1,7 +1,7 @@
 const Testimonial = () => {
   return (
-    <div className="bg-gray-100 py-24 text-gray-500">
-      <div className="max-w-xl mx-auto p-6">
+    <div className="bg-gray-100 py-20 text-gray-500">
+      <div className="max-w-xl mx-auto px-6">
         <p className="text-xl font-bold mb-4">
           Nonviolent communication workshop
         </p>
@@ -13,6 +13,7 @@ const Testimonial = () => {
           vitae venenatis turpis semper. Id leo fermentum est enim. Elit cras
           accumsan at id lacinia ac ut.
         </p>
+        <p className="font-bold mt-4">— Alizeh Khan, Frontend Engineer</p>
         <img src="/hellofresh.png" className="h-10 mt-4" alt="" />
       </div>
     </div>
